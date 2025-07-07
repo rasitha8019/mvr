325e25f2c1cd762f20beb86b8ba0073a7c248e97
Rasitha <ro200586@rguktong.ac.in>
2025-07-07 11:49:19 +0530

    first commit

--- backend/server.js
+++ backend/server.js
@@ -8,12 +8,8 @@ dotenv.config();
 const app = express();
 app.use(cors());
 app.use(express.json());
-
 mongoose
-  .connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+  .connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB connected"))
   .catch((err) => console.error("MongoDB error:", err));
 
